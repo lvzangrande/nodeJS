da90d47d64ab3b063789c75af18802a97d35d544
lvzangrande <zangrande360@gmail.com>
2026-08-28 16:09:32 -0300

    Resolvedo exercício 3

--- exercicios/Atividade03/index.js
+++ exercicios/Atividade03/index.js
@@ -1,8 +1,13 @@
 const nomeInformado = process.argv[2];
 const turmaInformada = process.argv[3];
 
-console.log('Entrada recebida:', { nomeInformado, turmaInformada });
-console.error('PENDENTE: valide nome e turma antes de confirmar a inscrição.');
+if(!nomeInformado && !turmaInformada){
+    
+console.error('ERRO! -> informe seu nome e turma para realizar a inscrição.');
 process.exitCode = 1;
+}
+else{
+console.log('Inscrição finalizada com acesso:', { nomeInformado, turmaInformada });
+}
 
 // TODO: substitua as três linhas acima pela validação e pela confirmação pedidas.
