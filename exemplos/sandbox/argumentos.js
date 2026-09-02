80ec7ac6d2ffb7dd4815bc5e87076a5b3d4cd6bc
lvzangrande <zangrande360@gmail.com>
2026-09-02 14:38:48 -0300

    Reorganizando arquivos de exercicio e adicionando exercício01 da aula002

--- exemplos/sandbox/argumentos.js
+++
@@ -1,9 +0,0 @@
-const [,,nome,turma] = process.argv;
-
-if(!nome || !turma){
-    console.error('Use: node argumentos.js <nome> <turma>');
-    process.exitCode=1;
-}
-else{
-    console.log({nome, turma});
-}
--- exemplos/sandbox/runtime.js
+++
@@ -1,8 +0,0 @@
-console.log({
-    node:process.version,
-    plataforma:process.platform,
-    arquitetura:process.arch,
-    diretorio:process.cwd()
-})
-
-process.exitCode = 2;
